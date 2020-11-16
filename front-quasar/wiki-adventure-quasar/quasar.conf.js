39adbb42ca2fb88a6c3cbac0b15ef788870c531d
Sacramentix <benji.u1225u@gmail.com>
2020-11-16 18:38:40 +0100

    port change

--- front-quasar/wiki-adventure-quasar/quasar.conf.js
+++ front-quasar/wiki-adventure-quasar/quasar.conf.js
@@ -72,8 +72,8 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
-      open: true // opens browser window automatically
+      port: 5001,
+      open: false // opens browser window automatically
     },
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
