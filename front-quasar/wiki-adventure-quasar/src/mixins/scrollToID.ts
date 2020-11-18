6adbd542bd0065269886766048243e25a73a8512
Sacramentix <benji.u1225u@gmail.com>
2020-11-18 23:28:59 +0100

    add scroll container parameter

--- front-quasar/wiki-adventure-quasar/src/mixins/scrollToID.ts
+++ front-quasar/wiki-adventure-quasar/src/mixins/scrollToID.ts
@@ -1,7 +1,13 @@
-export default function scrollToID(id:string) {
+export default function scrollToID(id:string, scrollContainer?:Element) {
     var element = document.getElementById(id);
     if (!element) return;
     var top = element.offsetTop;
     top = top+document.documentElement.clientHeight*0.88;
-    window.scrollTo(0, top);
+    if (!scrollContainer) {
+        window.scrollTo(0, top);
+    } else {
+        scrollContainer.scrollTo(0, top);
+    }
+    
+    console.log(id + " scroll");
 }
