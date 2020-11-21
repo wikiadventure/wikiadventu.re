2dbb15dcd29ae2568d1a0db56702fb261a40a6ef
Sacramentix <benji.u1225u@gmail.com>
2020-11-21 16:51:49 +0100

    french traduction

---
+++ front-quasar/wiki-adventure-quasar/src/i18n/fr/index.ts
@@ -0,0 +1,37 @@
+export default {
+    play: "jouer",
+    playSection: {
+      core: "Need to write this"
+    },
+    news: "news",
+    contribution: "contribution",
+    contributionSection: {
+      discord: "rejoins la commu sur Discord",
+      github: "contribue au jeu sur Github",
+      kofi: "offre moi un café",
+      nano: "fait don de quelque Nano"
+    },
+    home: "accueil",
+    publicLobby: "lobby public",
+    privateLobby: "lobby privé",
+    twitchLobby: "lobby Twitch",
+    join: "rejoindre",
+    create: "créer",
+    input: {
+      langSelect: "sélectionne une langue",
+      lobbyID: "lobby id",
+      pseudo: "pseudo",
+      password: "mot de passe"
+    },
+    menu: {
+      joinPublicLobby: "rejoins un lobby public",
+      joinPrivateLobby: "rejoins un lobby privé",
+      joinTwitchLobby: "rejoins un lobby Twitch",
+      createPrivateLobby: "crée un lobby privé",
+      createTwitchLobby: "crée un lobby Twitch",
+      button: {
+        createWithTwitch: "crée avec Twitch",
+        joinWithTwitch: "rejoins avec Twitch",
+      }
+    }
+  };
