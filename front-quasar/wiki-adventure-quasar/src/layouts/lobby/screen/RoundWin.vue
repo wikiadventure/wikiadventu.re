70f4cd3aacf0b401d30423bc9253c4cd9ffdaa10
Sacramentix <benji.u1225u@gmail.com>
2020-11-21 21:52:54 +0100

    remove initNamespace

---
+++ front-quasar/wiki-adventure-quasar/src/layouts/lobby/screen/RoundWin.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="round-win">
+    <logo-show-in :title="winner" ></logo-show-in>
+  </div>
+</template>
+<style lang="scss">
+.round-win {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(0,0,0,0.5);
+  z-index: 10;
+  transition: all ease-in-out 0.2s;
+}
+</style>
+<script lang="ts">
+import LogoShowIn from '../../../components/art/LogoShowIn.vue';
+
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'RoundWin',
+  components: { LogoShowIn },
+  props: {
+    winner:String
+  }
+});
+</script>
--- front-quasar/wiki-adventure-quasar/src/pages/Game.vue
+++ front-quasar/wiki-adventure-quasar/src/pages/Game.vue
@@ -2,38 +2,44 @@
   <div>
     <game-slide-menu/>
     <wiki-page ref="wikiPage"/>
+    <round-win v-if="showWinRound" :winner="winner"></round-win>
   </div>
 </template>
 <script lang="ts">
 import GameSlideMenu from "../layouts/lobby/GameSlideMenu.vue";
 import WikiPage from "../layouts/lobby/WikiPage.vue";
+import RoundWin from "../layouts/lobby/screen/RoundWin.vue"
 
 import { defineComponent } from '@vue/composition-api';
+import { Player } from "../store/gameData/state";
 
 export default defineComponent({
   name: 'Index',
-  components: { GameSlideMenu, WikiPage },
-  data():{
+  components: { GameSlideMenu, WikiPage, RoundWin },
+  data(): {
+    showWinRound:boolean,
+    winner:string,
     unsubscribe:() => void
   } {
     return {
+      showWinRound: false,
+      winner: "",
       unsubscribe: () => {}
     }
   },
   mounted() {
     this.$store.dispatch('gameData/connect');
   },
-  methods: {
-    getStartingPage(url:string) {
-      this.$emit('id-selected', 1)
-    }
-  },
   created() {
-    /*this.unsubscribe = this.$store.subscribe((mutation, state) => {
-      if (mutation.type === 'gameData/voteResult') {
-        this.getStartingPage(state.gameData.startPage);
+    this.unsubscribe = this.$store.subscribeAction((action, state) => {
+      if (action.type === 'gameData/onWinRound') {
+        var playerList = this.$store.state.gameData.players as Player[];
+        var player = playerList.find(p => p.id > action.payload!.id);
+        this.winner = player!.pseudo;
+        this.showWinRound = true;
+        setTimeout(() => {this.showWinRound = false}, 4000);
       }
-    });*/
+    });
   },
   beforeDestroy() {
     this.unsubscribe!();
