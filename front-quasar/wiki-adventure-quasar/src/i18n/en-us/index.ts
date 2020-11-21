1021f724429ec8c60ccb7c1c4855e46377cdcbda
Sacramentix <benji.u1225u@gmail.com>
2020-11-21 16:51:29 +0100

    english traduction

--- front-quasar/wiki-adventure-quasar/src/i18n/en-us/index.ts
+++ front-quasar/wiki-adventure-quasar/src/i18n/en-us/index.ts
@@ -1,7 +1,37 @@
-// This is just an example,
-// so you can safely delete all default props below
-
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
+  play: "play",
+  playSection: {
+    core: "Need to write this"
+  },
+  news: "news",
+  contribution: "contribution",
+  contributionSection: {
+    discord: "join the community on Discord",
+    github: "contribute to the game on Github",
+    kofi: "buy me a coffee",
+    nano: "donate some Nano"
+  },
+  home: "home",
+  publicLobby: "public lobby",
+  privateLobby: "private lobby",
+  twitchLobby: "twitch lobby",
+  join: "join",
+  create: "create",
+  input: {
+    langSelect: "select a language",
+    lobbyID: "lobby id",
+    pseudo: "pseudo",
+    password: "password"
+  },
+  menu: {
+    joinPublicLobby: "join a public lobby",
+    joinPrivateLobby: "join a private lobby",
+    joinTwitchLobby: "join a Twitch lobby",
+    createPrivateLobby: "create a private lobby",
+    createTwitchLobby: "create a Twitch lobby",
+    button: {
+      createWithTwitch: "create with Twitch",
+      joinWithTwitch: "join with Twitch",
+    }
+  }
 };
