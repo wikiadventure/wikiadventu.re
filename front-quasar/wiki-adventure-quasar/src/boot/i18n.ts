8aaf37f5e4721c439c8472f01b419416b87ada8e
Sacramentix <benji.u1225u@gmail.com>
2020-11-30 17:09:05 +0100

    default to en

--- front-quasar/wiki-adventure-quasar/src/boot/i18n.ts
+++ front-quasar/wiki-adventure-quasar/src/boot/i18n.ts
@@ -12,8 +12,8 @@ declare module 'vue/types/vue' {
 Vue.use(VueI18n);
 
 export const i18n = new VueI18n({
-  locale: "en-us",
-  fallbackLocale: "en-us",
+  locale: "en",
+  fallbackLocale: "en",
   messages
 });
 
