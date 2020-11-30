bd9989985eb89ad00647236933a1b17059d28c0d
Sacramentix <benji.u1225u@gmail.com>
2020-11-30 17:16:08 +0100

    volume slider

---
+++ front-quasar/wiki-adventure-quasar/src/components/setting/VolumeSlider.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-item>
+    <q-item-section avatar>
+      <q-btn round dense flat :icon=" mute ? 'mdi-volume-off' : 'mdi-volume-high'" class="chatSubmit" @click="mute = !mute"/>
+
+    </q-item-section>
+    <q-item-section>
+      <q-slider v-model="volume" :min="0" :max="1" />
+    </q-item-section>
+  </q-item>
+</template>
+<style lang="scss">
+
+</style>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'VolumeSlider',
+  computed: {
+    volume: {
+      get() {
+        return this.$store.state.gameData.volume;
+      },
+      set(v:number) {
+        this.$store.commit('gameData/volume', v);
+      }
+    },
+    mute: {
+      get() {
+        return this.$store.state.gameData.mute;
+      },
+      set(m:number) {
+        this.$store.commit('gameData/mute', m);
+      }
+    }
+  }
+
+});
+</script>
