d6c24c48d53d5358fa72edd39132aaf76b79b714
Sacramentix <benji.u1225u@gmail.com>
2020-11-30 17:15:41 +0100

    translation lang switch

---
+++ front-quasar/wiki-adventure-quasar/src/components/setting/LangSwitch.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <q-select @input="onSelect($event)" outlined
+    v-model="model" :options="options"
+    :value="getLang" :display-value="getLangLabel"
+    v-bind="$attrs" v-on="$listeners" behavior="dialog">
+    <template v-slot:prepend>
+      <q-icon name="mdi-translate"></q-icon>
+    </template>
+  </q-select>
+</template>
+<style lang="scss">
+</style>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import { getLabel, Lang } from '../../i18n';
+
+export default defineComponent({
+  name: 'LangSwitch',
+   data() {
+    return {
+      model: null
+    }
+  },
+  methods: {
+    onSelect(event:any) {
+      this.$i18n.locale = event.value;
+    }
+  },
+  computed: {
+    options():Array<{ value:string, label:string }> {
+      var optionsList:Array<{ value:string, label:string }> = [];
+      for (var v in Lang) {
+        var option:{ value:string, label:string } = {value: v, label: getLabel(v)};
+        optionsList.push(option);
+      }
+      return optionsList;
+
+    },
+    getLang():string {
+      return this.$i18n.locale;
+    },
+    getLangLabel():string {
+     return getLabel(this.$i18n.locale);
+    }
+  }
+});
+</script>
