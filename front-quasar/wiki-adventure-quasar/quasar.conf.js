4549816646074ca9b97c2a9b214d51f8fb11d8b6
Sacramentix <benji.u1225u@gmail.com>
2020-12-09 14:27:40 +0100

    add notify plugin

--- front-quasar/wiki-adventure-quasar/quasar.conf.js
+++ front-quasar/wiki-adventure-quasar/quasar.conf.js
@@ -95,7 +95,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Notify']
     },
 
     // animations: 'all', // --- includes all animations
