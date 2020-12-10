e4aa2d019298640d3cd34f155d036de5d5bd784b
Sacramentix <benji.u1225u@gmail.com>
2020-12-10 17:28:03 +0100

    NEW fullscreenSwitch

---
+++ front-quasar/wiki-adventure-quasar/src/components/setting/FullscreenSwitch.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-btn
+      @click="$q.fullscreen.toggle()"
+      :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
+      :label="$q.fullscreen.isActive ? 'Exit Fullscreen' : 'Go Fullscreen'"
+    />
+</template>
+<style lang="scss">
+</style>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import { getLabel, Lang } from '../../i18n';
+
+export default defineComponent({
+  name: 'FullscreenSwitch'
+});
+</script>
