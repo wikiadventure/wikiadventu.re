369aefec0839324ef6cde9e77398554c3bb562a8
Sacramentix <benji.u1225u@gmail.com>
2020-12-10 17:31:58 +0100

    margin + new setting

--- front-quasar/wiki-adventure-quasar/src/layouts/lobby/tab/SettingTabPanel.vue
+++ front-quasar/wiki-adventure-quasar/src/layouts/lobby/tab/SettingTabPanel.vue
@@ -1,23 +1,27 @@
 <template>
-  <div id="settingTab">
-      <div class="row"><h3 class="row text-center">Setting</h3></div>
-      <theme-switch/>
-      <lang-switch/>
-      <volume-slider/>
+  <div id="settingTab" class="col">
+    <div class="row justify-evenly q-ma-none"><h3>Setting</h3></div>
+    <div class="row justify-evenly"><theme-switch/><fullscreen-switch/></div>
+    <lang-switch/>
+    <volume-slider/>
   </div>
 </template>
 <style lang="scss">
+#settingTab>* {
+  margin: 15px 5px;
+}
 </style>
 <script lang="ts">
 import ThemeSwitch from '../../../components/setting/ThemeSwitch.vue';
 import LangSwitch from '../../../components/setting/LangSwitch.vue';
-import VolumeSlider from '../../../components/setting/VolumeSlider.vue'
+import VolumeSlider from '../../../components/setting/VolumeSlider.vue';
+import FullscreenSwitch from '../../../components/setting/FullscreenSwitch.vue';
 
 import { defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
   name: 'SettingTabPanel',
-  components: { ThemeSwitch, LangSwitch, VolumeSlider },
+  components: { ThemeSwitch, LangSwitch, VolumeSlider, FullscreenSwitch },
   data() {
     return {
     }
