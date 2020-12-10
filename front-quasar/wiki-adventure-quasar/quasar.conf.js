e4e4de3d5a42fe963057415f51768d7d0cd75bfc
Sacramentix <benji.u1225u@gmail.com>
2020-12-10 17:36:15 +0100

    add AppFullScreen

--- front-quasar/wiki-adventure-quasar/quasar.conf.js
+++ front-quasar/wiki-adventure-quasar/quasar.conf.js
@@ -95,7 +95,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Notify']
+      plugins: ['Notify','AppFullscreen']
     },
 
     // animations: 'all', // --- includes all animations
