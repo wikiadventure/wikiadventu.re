0acd2532b37f1a73a7196670dd5e684077cfe987
Sacramentix <benji.u1225u@gmail.com>
2021-01-05 20:42:49 +0100

    translation for game menu

--- front-quasar/wiki-adventure-quasar/src/i18n/en/index.ts
+++ front-quasar/wiki-adventure-quasar/src/i18n/en/index.ts
@@ -33,5 +33,34 @@ export default {
       createWithTwitch: "create with Twitch",
       joinWithTwitch: "join with Twitch",
     }
-  }
+  },
+  gameTab: {
+    round: "Round",
+    phase: "Phase",
+    timeLeft: "Time left",
+    vote: "Your vote",
+    submitVote: "Submit your vote",
+    startPage: "Start page",
+    endPage: "End page",
+    randomPage: "Random page ???"
+  },
+  chatTab: {
+    send: "Send a message"
+  },
+  settingTab: {
+    settings: "Settings"
+  },
+  phase: {
+    Voting: "Voting",
+    Playing: "Searching",
+    RoundFinish: "Result",
+    GameFinish: "Game end",
+    notify: {
+      Voting: "Voting phase started. You can now submit your suggestion!",
+      Playing: "Searching phase started. Be the first to reach the end page!"
+    }
+  },
+  exitWarn: "Do you really want to leave the lobby?",
+  noLobbyFound: "No lobby found with id",
+  fetchError: "An error occurred while communicating with server"
 };
--- front-quasar/wiki-adventure-quasar/src/i18n/fr/index.ts
+++ front-quasar/wiki-adventure-quasar/src/i18n/fr/index.ts
@@ -33,5 +33,34 @@ export default {
         createWithTwitch: "crée avec Twitch",
         joinWithTwitch: "rejoins avec Twitch",
       }
-    }
+    },
+    gameTab: {
+      round: "Round",
+      phase: "Phase",
+      timeLeft: "Temps restant",
+      vote: "Ton vote",
+      submitVote: "envoie ton vote",
+      startPage: "Page de départ",
+      endPage: "page d'arrivée",
+      randomPage: "Page aléatoire ???"
+    },
+    chatTab: {
+      send: "Envoie un message"
+    },
+    settingTab: {
+      settings: "Options"
+    },
+    phase: {
+      Voting: "Vote",
+      Playing: "Recherche",
+      RoundFinish: "Résultat",
+      GameFinish: "Fin de partie",
+      notify: {
+        Voting: "La phase de vote commence. Vous pouvez envoyer vos suggestion!",
+        Playing: "La phase de recherche commence. Soyez le premier à atteindre la page d'arrivé!"
+      }
+    },
+    exitWarn: "Voulez-vous vraiment quitter le lobby?",
+    noLobbyFound: "Aucun lobby trouvé avec l'id",
+    fetchError: "Une erreur a eu lieu lors de la communication avec le serveur"
   };
