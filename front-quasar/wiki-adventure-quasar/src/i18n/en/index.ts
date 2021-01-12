dcc7757e18dbf38ed84b0628b2c59c52faa2de0a
Arthur Madec-Prévost <contact@arthurmadecprevost.fr>
2021-01-12 02:48:43 +0100

    Update index.ts

--- front-quasar/wiki-adventure-quasar/src/i18n/en/index.ts
+++ front-quasar/wiki-adventure-quasar/src/i18n/en/index.ts
@@ -3,35 +3,35 @@ export default {
   playSection: {
     core: "Need to write this"
   },
-  news: "news",
+  news: "News",
   contribution: "contribution",
   contributionSection: {
-    discord: "join the community on Discord",
-    github: "contribute to the game on Github",
-    kofi: "buy me a coffee",
-    nano: "donate some Nano"
+    discord: "Find friends to play with on our Discord",
+    github: "Contribute to the game",
+    kofi: "Buy me a coffee",
+    nano: "Share your Nano with me !"
   },
-  home: "home",
-  publicLobby: "public lobby",
-  privateLobby: "private lobby",
-  twitchLobby: "twitch lobby",
-  join: "join",
-  create: "create",
+  home: "Home",
+  publicLobby: "Public Party",
+  privateLobby: "Private Party",
+  twitchLobby: "Twitch Party",
+  join: "Join",
+  create: "Create",
   input: {
-    langSelect: "select wiki language",
-    lobbyID: "lobby id",
-    pseudo: "pseudo",
-    password: "password"
+    langSelect: "Select Wikipedia language",
+    lobbyID: "Party ID",
+    pseudo: "Username",
+    password: "Password"
   },
   menu: {
-    joinPublicLobby: "join a public lobby",
-    joinPrivateLobby: "join a private lobby",
-    joinTwitchLobby: "join a Twitch lobby",
-    createPrivateLobby: "create a private lobby",
-    createTwitchLobby: "create a Twitch lobby",
+    joinPublicLobby: "Join a Public Party",
+    joinPrivateLobby: "Join a Private Party",
+    joinTwitchLobby: "Join a Twitch Party",
+    createPrivateLobby: "Create a Private Party",
+    createTwitchLobby: "Create a Twitch Party",
     button: {
-      createWithTwitch: "create with Twitch",
-      joinWithTwitch: "join with Twitch",
+      createWithTwitch: "Create with Twitch",
+      joinWithTwitch: "Join with Twitch",
     }
   },
   gameTab: {
@@ -40,7 +40,7 @@ export default {
     timeLeft: "Time left",
     vote: "Your vote",
     submitVote: "Submit your vote",
-    startPage: "Start page",
+    startPage: "Start Page",
     endPage: "End page",
     randomPage: "Random page ???"
   },
@@ -57,13 +57,13 @@ export default {
     RoundFinish: "Result",
     GameFinish: "Game end",
     notify: {
-      Voting: "Voting phase started. You can now submit your suggestion!",
+      Voting: "Voting phase started. You can now submit your suggestions!",
       Playing: "Searching phase started. Be the first to reach the end page!"
     }
   },
-  exitWarn: "Do you really want to leave the lobby?",
-  noLobbyFound: "No lobby found with id",
-  lobbyIDRequired: "Please enter a lobby id to connect to",
+  exitWarn: "Do you really want to leave the party?",
+  noLobbyFound: "No game found with this Party ID",
+  lobbyIDRequired: "Please enter a Party ID to connect to",
   fetchError: "An error occurred while communicating with server",
   copySuccess: "Copied successfully!",
   copyFail: "Error during the copy"
