5846202aa3899c9f61be2fdc8482fb2818065716
Sacramentix <benji.u1225u@gmail.com>
2021-01-13 19:11:59 +0100

    setup file

---
+++ setup.bat
@@ -0,0 +1,17 @@
+haxelib git twitch-auth https://github.com/Sacramentix/hx-twitch-auth.git --always
+haxelib git twitch-api-call https://github.com/Sacramentix/hx-twitch-api-call.git --always
+haxelib git d-fischer-logger https://github.com/Sacramentix/hx-d-fischer-logger.git --always
+haxelib git d-fischer-shared-utils https://github.com/Sacramentix/hx-d-fischer-shared-utils.git --always
+haxelib git d-fischer-typed-event-emitter https://github.com/Sacramentix/hx-d-fischer-typed-event-emitter.git --always
+haxelib git ws https://github.com/Sacramentix/hx-ws.git --always
+haxelib install uuid --always
+haxelib install tink_json --always
+haxelib git d-fischer-connection https://github.com/Sacramentix/hx-d-fischer-connection.git --always
+haxelib git twitch https://github.com/Sacramentix/hx-twitch.git --always
+haxelib git ircv3 https://github.com/Sacramentix/hx-ircv3.git --always
+haxelib git twitch-chat-client https://github.com/Sacramentix/hx-twitch-chat-client.git --always
+cd bin
+npm install
+cd ../front-quasar/wiki-adventure-quasar
+npm install
+npm install -g @quasar/cli
