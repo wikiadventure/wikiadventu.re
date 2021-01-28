9e5d87d881c13e4fd95974bc4f4616bc642881a5
Sacramentix <benji.u1225u@gmail.com>
2021-01-28 19:29:44 +0100

    icon for owner and disconnected player

--- front-quasar/wiki-adventure-quasar/src/layouts/lobby/tab/ScoreTabPanel.vue
+++ front-quasar/wiki-adventure-quasar/src/layouts/lobby/tab/ScoreTabPanel.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="scoreTab">
-    <div class="row items-baseline q-ma-sm"><h4 class="q-ma-none flex-fix">{{ $t('scoreTab.title') }}</h4></div>
-    <q-separator  spaced="md"/>
-    <div class="row items-baseline q-ma-sm"><q-btn class="action-btn" push :label="$t('scoreTab.leaderboard')" icon="mdi-format-list-bulleted" @click="openLeaderBoard()"/></div>
-    <q-separator  spaced="md"/>
+    <div class="row items-baseline justify-evenly"><h4>{{ $t('scoreTab.title') }}</h4></div>
+    <q-separator spaced="md"/>
+    <div class="row items-baseline justify-evenly"><q-btn class="action-btn" push :label="$t('scoreTab.leaderboard')" icon="mdi-format-list-bulleted" @click="openLeaderBoard()"/></div>
+    <q-separator spaced="md"/>
     <q-list separator>
       <q-item v-for="player in playersByScore" :key="player.id">
-        <q-item-section avatar>
-          <q-avatar>
-            <!--<img src="https://cdn.quasar.dev/img/boy-avatar.png">-->
-          </q-avatar>
+        <q-item-section avatar>            
+          <q-icon v-if="isOwner(player.id)" color="teal" name="mdi-crown"></q-icon>
+          <q-icon v-else-if="!player.isConnected" color="red" name="mdi-wifi-off"></q-icon>
         </q-item-section>
-        <q-item-section>{{ player.pseudo }}</q-item-section>
+        <q-item-section :class="{ self: isSelf(player.id) }">{{ player.pseudo }}</q-item-section>
         <q-item-section>{{ player.score }}</q-item-section>
       </q-item>
-      <!--<q-expansion-item v-for="player in players" :key="player.id" :label="player.pseudo">
-        <q-card>
-          <q-card-section>
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>-->
     </q-list>
   </div>
 </template>
 <style lang="scss">
+.self {
+  color: var(--w-color-dark-teal);
+}
 .body--dark .action-btn {
   background: var(--w-color-dark-teal);
   color: var(--w-color-dark-blue);          
@@ -56,6 +52,12 @@ export default defineComponent({
         state: true
       }
       this.$root.$emit("manage-screen", payload);
+    },
+    isSelf(id:number):boolean {
+      return this.$store.state.gameData.self == id;
+    },
+    isOwner(id:number):boolean {
+      return this.$store.state.gameData.owner == id;
     }
   }
 });
