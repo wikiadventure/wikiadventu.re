f36bd47ea14d8920e907be7c21b1659c98fb7830
Sacramentix <benji.u1225u@gmail.com>
2021-03-02 12:26:08 +0100

    lobbyPhase -> gamePhase

--- front-quasar/wiki-adventure-quasar/src/layouts/lobby/tab/GameTabPanel.vue
+++ front-quasar/wiki-adventure-quasar/src/layouts/lobby/tab/GameTabPanel.vue
@@ -1,11 +1,29 @@
 <template>
   <div id="gameTab">
-    <div class="row q-ma-md"><h3 class="q-ma-none col-grow text-center">{{ $t('gameTab.round') }} {{ round }}</h3></div>
-    <div class="row items-baseline q-ma-sm"><h4 class="q-ma-none col-strech">{{ $t('gameTab.phase') }}</h4><h5 class="q-ma-none q-ml-sm text-center col-grow">{{ $t('phase.'+lobbyPhase) }}</h5></div>
+
+    <div class="row justify-evenly q-ma-md text-h3">{{ $t('gameTab.round') }} {{ round }}</div>
+
     <q-separator spaced="lg"/>
-    <div class="row items-baseline q-ma-sm"><h4 class="q-ma-none col-strech">{{ $t('gameTab.timeLeft') }}</h4><h5 class="q-ma-none q-ml-sm text-center col-grow">{{ timeLeft }}</h5></div>
+
+    <div class="row items-baseline q-ma-sm">
+      <h4 class="q-ma-none col-strech">{{ $t('gameTab.phase') }}</h4>
+      <h5 class="q-ma-none q-ml-sm text-center col-grow">{{ $t('phase.'+gamePhase) }}</h5>
+    </div>
+
+    <q-separator spaced="lg"/>
+
+    <div class="row items-baseline q-ma-sm">
+      <h4 class="q-ma-none col-strech">{{ $t('gameTab.timeLeft') }}</h4>
+      <h5 class="q-ma-none q-ml-sm text-center col-grow">{{ timeLeft }}</h5>
+    </div>
+
     <q-separator  spaced="lg"/>
-    <div id="vote" class="row items-baseline q-ma-sm"><p class="q-ma-none col-strech">{{ $t('gameTab.vote') }} :</p><p class="q-ma-none q-ml-sm text-left col-grow">{{ vote }}</p><q-btn round dense flat icon="mdi-delete" class="voteDelete" @click="resetVote()"></q-btn></div>
+
+    <div id="vote" class="row items-baseline q-ma-sm">
+      <p class="q-ma-none col-strech">{{ $t('gameTab.vote') }} :</p>
+      <p class="q-ma-none q-ml-sm text-left col-grow">{{ vote }}</p>
+      <q-btn round dense flat icon="mdi-delete" class="voteDelete" @click="resetVote()"></q-btn>
+    </div>
     <q-form class="voteForm q-ma-sm">
       <q-input dense class="voteInput" @keydown.enter.prevent="submitVote()" maxlength="255" outlined v-model="voteInput" :label="$t('gameTab.submitVote')" spellcheck="false">
         <template v-slot:after>
@@ -13,14 +31,34 @@
         </template>
       </q-input>
     </q-form>
+
     <q-separator spaced="lg"/>
-    <div class="row items-baseline q-ma-sm"><p class="q-ma-none col-strech">{{ $t('gameTab.startPage') }} :</p><p class="q-ma-none q-ml-sm text-left col-grow">{{ startPage }}</p></div>
-    <div class="row items-baseline q-ma-sm"><p class="q-ma-none col-strech">{{ $t('gameTab.endPage') }} :</p><p class="q-ma-none q-ml-sm text-left col-grow">{{ endPage }}</p></div>
+
+    <div class="row items-baseline q-ma-sm">
+      <p class="q-ma-none col-strech">{{ $t('gameTab.startPage') }} :</p>
+      <p class="q-ma-none q-ml-sm text-left col-grow">{{ startPage }}</p>
+    </div>
+    <div class="row items-baseline q-ma-sm">
+      <p class="q-ma-none col-strech">{{ $t('gameTab.endPage') }} :</p>
+      <p class="q-ma-none q-ml-sm text-left col-grow">{{ endPage }}</p>
+    </div>
+
     <q-separator  spaced="lg"/>
-    <div class="row items-baseline justify-evenly"><q-btn class="action-btn q-ma-sm" push :label="$t('gameTab.pageHistory')" icon="mdi-format-list-bulleted" @click="open('page-history')"/><q-btn class="action-btn q-ma-sm" push :label="$t('gameTab.showEndPage')" icon="mdi-page-next-outline" @click="open('wiki-end-page')"/></div>
+
+    <div class="row items-baseline justify-evenly">
+      <q-btn class="action-btn q-ma-sm" push :label="$t('gameTab.pageHistory')" icon="mdi-format-list-bulleted" @click="open('page-history')"/>
+      <q-btn class="action-btn q-ma-sm" push :label="$t('gameTab.showEndPage')" icon="mdi-page-next-outline" @click="open('wiki-end-page')"/>
+    </div>
+
     <q-separator  spaced="lg"/>
-    <div class="row items-baseline justify-evenly"><p>{{ totalVoteSkip }} / {{ connectedPlayers }}</p><q-btn class="action-btn q-ma-sm" push label="skip" icon="mdi-skip-forward" @click="voteSkip()"/></div>
+
+    <div class="row items-baseline justify-evenly">
+      <p>{{ totalVoteSkip }} / {{ connectedPlayers }}</p>
+      <q-btn class="action-btn q-ma-sm" push label="skip" icon="mdi-skip-forward" @click="voteSkip()"/>
+    </div>
+    
     <q-separator spaced="lg"/>
+
   </div>
 </template>
 <style lang="scss">
@@ -58,8 +96,8 @@ export default defineComponent({
     endPage():string {
       return this.$store.state.gameData.endPage;
     },
-    lobbyPhase():string {
-      return this.$store.state.gameData.lobbyPhase;
+    gamePhase():number {
+      return this.$store.state.gameData.gamePhase;
     },
     round():string {
       return this.$store.state.gameData.round;
