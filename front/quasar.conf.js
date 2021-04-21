9e5dee152ceffd1a9bb0a9f56bdb6f833e17bba3
Sacramentix <benji.u1225u@gmail.com>
2021-04-21 19:07:54 +0200

    source map in config

--- front/quasar.conf.js
+++ front/quasar.conf.js
@@ -63,7 +63,7 @@ module.exports = configure(function (/* ctx */) {
 
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
-
+      devtool: 'source-map',
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack (cfg) {
       },
