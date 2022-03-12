23a3693c0d5aee3266830a317440a9da7afdeef5
Sacramentix <benji.u1225u@gmail.com>
2022-03-12 22:11:21 +0100

    add vue i18 loader

--- front/quasar.conf.js
+++ front/quasar.conf.js
@@ -88,7 +88,12 @@ module.exports = configure(function (ctx) {
                 .loader('image-webpack-loader')
                 .end()
               .end()
-            .end()
+            .rule("i18n")
+              .resourceQuery(/blockType=i18n/)
+              .type('javascript/auto')
+              .use("i18n")
+                .loader("@intlify/vue-i18n-loader")
+                .end();
       }
     },
 
