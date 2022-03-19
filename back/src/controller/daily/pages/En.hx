cb43f71a3ed638da72dd68c8ccb4dc5f1a0dac9b
Sacramentix <benji.u1225u@gmail.com>
2022-03-19 17:54:42 +0100

    add daily for next week

--- back/src/controller/daily/pages/En.hx
+++ back/src/controller/daily/pages/En.hx
@@ -6,8 +6,15 @@ import controller.daily.DailyPage.DailyDate;
 class En {
     
     public static final dailyPages:Map<DailyDate, DailyPageType> = [
-        "09/03/2022" => { start: "Paris", end: "France" },
-        "10/03/2022" => { start: "France", end: "Paris" },
+        "19/03/2022" => { start: "Wikipedia", end: "Six_Degrees_of_Kevin_Bacon" },
+        "20/03/2022" => { start: "Wikipedia", end: "Six_Degrees_of_Kevin_Bacon" },
+        "21/03/2022" => { start: "Paleolithic", end: "Colonization_of_Mars" },
+        "22/03/2022" => { start: "Green_anaconda", end: "Cryovolcano" },
+        "23/03/2022" => { start: "Ultra-Trail_du_Mont-Blanc", end: "Wingsuit_flying" },
+        "24/03/2022" => { start: "Gold_rush", end: "Ocean_disposal_of_radioactive_waste" },
+        "25/03/2022" => { start: "Userkaf", end: "Sexagenary_cycle" },
+        "26/03/2022" => { start: "Hit_the_Road_Jack", end: "World_Chase_Tag" },
+        "27/03/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
     ];
 
 }
--- back/src/controller/daily/pages/Fr.hx
+++ back/src/controller/daily/pages/Fr.hx
@@ -6,8 +6,15 @@ import controller.daily.DailyPage.DailyDate;
 class Fr {
     
     public static final dailyPages:Map<DailyDate, DailyPageType> = [
-        "09/03/2022" => { start: "Paris", end: "France" },
-        "10/03/2022" => { start: "Saguaro", end: "Informatique_durable" },
+        "19/03/2022" => { start: "Wikipédia", end: "Ruée_vers_l%27or" },
+        "20/03/2022" => { start: "Wikipédia", end: "Six_Degrees_of_Kevin_Bacon" },
+        "21/03/2022" => { start: "Paléolithique", end: "Colonisation_de_Mars" },
+        "22/03/2022" => { start: "Eunectes_murinus", end: "Cryovolcan" },
+        "23/03/2022" => { start: "Ultra-Trail_du_Mont-Blanc", end: "Vol_en_wingsuit" },
+        "24/03/2022" => { start: "Ruée_vers_l%27or", end: "Déchet_radioactif" },
+        "25/03/2022" => { start: "Ouserkaf", end: "Cycle_sexagésimal_chinois" },
+        "26/03/2022" => { start: "Hit_the_Road_Jack", end: "International_Commerce_Center" },
+        "27/03/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
     ];
 
 }
