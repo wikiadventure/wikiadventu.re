77bd6c3c91c42cc443e2963aaff270df87bdc99e
Sacramentix <benji.u1225u@gmail.com>
2022-03-28 16:01:17 +0200

    fix date on fr daily

--- back/src/controller/daily/pages/Fr.hx
+++ back/src/controller/daily/pages/Fr.hx
@@ -10,10 +10,10 @@ class Fr {
         "29/03/2022" => { start: "Paléolithique", end: "Colonisation_de_Mars" },
         "30/03/2022" => { start: "Eunectes_murinus", end: "Cryovolcan" },
         "31/03/2022" => { start: "Ultra-Trail_du_Mont-Blanc", end: "Vol_en_wingsuit" },
-        "01/03/2022" => { start: "Ruée_vers_l%27or", end: "Déchet_radioactif" },
-        "02/03/2022" => { start: "Ouserkaf", end: "Cycle_sexagésimal_chinois" },
-        "03/03/2022" => { start: "Hit_the_Road_Jack", end: "International_Commerce_Center" },
-        "04/03/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
+        "01/04/2022" => { start: "Ruée_vers_l%27or", end: "Déchet_radioactif" },
+        "02/04/2022" => { start: "Ouserkaf", end: "Cycle_sexagésimal_chinois" },
+        "03/04/2022" => { start: "Hit_the_Road_Jack", end: "International_Commerce_Center" },
+        "04/04/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
     ];
 
 }
