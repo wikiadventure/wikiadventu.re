536a26f71f72bfee7eb1580dae6585a0d172f6d7
Sacramentix <benji.u1225u@gmail.com>
2022-03-28 16:00:44 +0200

    change daily

--- back/src/controller/daily/pages/Fr.hx
+++ back/src/controller/daily/pages/Fr.hx
@@ -6,15 +6,14 @@ import controller.daily.DailyPage.DailyDate;
 class Fr {
     
     public static final dailyPages:Map<DailyDate, DailyPageType> = [
-        "19/03/2022" => { start: "Wikipédia", end: "Ruée_vers_l%27or" },
-        "20/03/2022" => { start: "Wikipédia", end: "Six_Degrees_of_Kevin_Bacon" },
-        "21/03/2022" => { start: "Paléolithique", end: "Colonisation_de_Mars" },
-        "22/03/2022" => { start: "Eunectes_murinus", end: "Cryovolcan" },
-        "23/03/2022" => { start: "Ultra-Trail_du_Mont-Blanc", end: "Vol_en_wingsuit" },
-        "24/03/2022" => { start: "Ruée_vers_l%27or", end: "Déchet_radioactif" },
-        "25/03/2022" => { start: "Ouserkaf", end: "Cycle_sexagésimal_chinois" },
-        "26/03/2022" => { start: "Hit_the_Road_Jack", end: "International_Commerce_Center" },
-        "27/03/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
+        "28/03/2022" => { start: "Wikipédia", end: "Six_Degrees_of_Kevin_Bacon" },
+        "29/03/2022" => { start: "Paléolithique", end: "Colonisation_de_Mars" },
+        "30/03/2022" => { start: "Eunectes_murinus", end: "Cryovolcan" },
+        "31/03/2022" => { start: "Ultra-Trail_du_Mont-Blanc", end: "Vol_en_wingsuit" },
+        "01/03/2022" => { start: "Ruée_vers_l%27or", end: "Déchet_radioactif" },
+        "02/03/2022" => { start: "Ouserkaf", end: "Cycle_sexagésimal_chinois" },
+        "03/03/2022" => { start: "Hit_the_Road_Jack", end: "International_Commerce_Center" },
+        "04/03/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
     ];
 
 }
