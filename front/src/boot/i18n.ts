70197bf96c41235a1a4757e59d4479f1ccf3282c
Sacramentix <benji.u1225u@gmail.com>
2022-03-28 16:08:08 +0200

    default to locale

--- front/src/boot/i18n.ts
+++ front/src/boot/i18n.ts
@@ -5,8 +5,9 @@ import messages, { Lang } from 'src/i18n';
 import { Quasar } from 'quasar';
 import type { MessageSchema, SupportedLang } from "src/i18n";
 
-var localLang = Quasar.lang.getLocale();
-var computedLocalLang = localLang?.split("-")[0] || "en";
+const localLang = Quasar.lang.getLocale();
+var computedLocalLang = (localLang?.split("-")[0] || Lang.en) as Lang;
+if (!Object.values(Lang).includes(computedLocalLang)) computedLocalLang = Lang.en;
 
 const i18n = createI18n<[MessageSchema], SupportedLang>({
   //globalInjection: true,
--- front/src/store/connect/state.ts
+++ front/src/store/connect/state.ts
@@ -2,12 +2,13 @@ import { ref } from "vue";
 import { Lang } from "src/i18n";
 import { GameLoopType, VanillaLoopType } from "store/lobby/game/loop/type";
 import { Quasar } from "quasar";
+import { i18n } from "src/boot/i18n";
 
-var localLang = Quasar.lang.getLocale();
+const localLang = Quasar.lang.getLocale();
 var computedLocalLang = (localLang?.split("-")[0] || Lang.en) as Lang;
 if (!Object.values(Lang).includes(computedLocalLang)) computedLocalLang = Lang.en;
 
-export const lang = ref<Lang>(Lang.en);
+export const lang = ref<Lang>(computedLocalLang);
 export const pseudo = ref("");
 export const id = ref("");
 export const gameLoop = ref<GameLoopType>(VanillaLoopType.Classic);
