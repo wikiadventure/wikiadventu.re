193f8ac9b949e3f7fe4cba77aaf9d48aff234b20
Sacramentix <benji.u1225u@gmail.com>
2022-04-03 19:32:33 +0200

    new daily

--- back/src/controller/daily/pages/En.hx
+++ back/src/controller/daily/pages/En.hx
@@ -6,14 +6,14 @@ import controller.daily.DailyPage.DailyDate;
 class En {
     
     public static final dailyPages:Map<DailyDate, DailyPageType> = [
-        "28/03/2022" => { start: "Wikipedia", end: "Six_Degrees_of_Kevin_Bacon" },
-        "29/03/2022" => { start: "Paleolithic", end: "Colonization_of_Mars" },
-        "30/03/2022" => { start: "Green_anaconda", end: "Cryovolcano" },
-        "31/03/2022" => { start: "Ultra-Trail_du_Mont-Blanc", end: "Wingsuit_flying" },
-        "01/04/2022" => { start: "Gold_rush", end: "Ocean_disposal_of_radioactive_waste" },
-        "02/04/2022" => { start: "Userkaf", end: "Sexagenary_cycle" },
-        "03/04/2022" => { start: "Hit_the_Road_Jack", end: "World_Chase_Tag" },
-        "04/04/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
+        "03/04/2022" => { start: "April_Fools%27_Day", end: "Place_(Reddit)" },
+        "04/04/2022" => { start: "Hit_the_Road_Jack", end: "World_Chase_Tag" },
+        "05/04/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
+        "06/04/2022" => { start: "Wikipedia", end: "Six_Degrees_of_Kevin_Bacon" },
+        "07/04/2022" => { start: "Paleolithic", end: "Colonization_of_Mars" },
+        "08/04/2022" => { start: "Green_anaconda", end: "Cryovolcano" },
+        "09/04/2022" => { start: "Ultra-Trail_du_Mont-Blanc", end: "Wingsuit_flying" },
+        "10/04/2022" => { start: "Userkaf", end: "Sexagenary_cycle" },
     ];
 
 }
--- back/src/controller/daily/pages/Fr.hx
+++ back/src/controller/daily/pages/Fr.hx
@@ -6,14 +6,14 @@ import controller.daily.DailyPage.DailyDate;
 class Fr {
     
     public static final dailyPages:Map<DailyDate, DailyPageType> = [
-        "28/03/2022" => { start: "Wikipédia", end: "Six_Degrees_of_Kevin_Bacon" },
-        "29/03/2022" => { start: "Paléolithique", end: "Colonisation_de_Mars" },
-        "30/03/2022" => { start: "Eunectes_murinus", end: "Cryovolcan" },
-        "31/03/2022" => { start: "Ultra-Trail_du_Mont-Blanc", end: "Vol_en_wingsuit" },
-        "01/04/2022" => { start: "Ruée_vers_l%27or", end: "Déchet_radioactif" },
-        "02/04/2022" => { start: "Ouserkaf", end: "Cycle_sexagésimal_chinois" },
-        "03/04/2022" => { start: "Hit_the_Road_Jack", end: "International_Commerce_Center" },
-        "04/04/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
+        "03/04/2022" => { start: "Place_(Reddit)", end: "Arte" },
+        "04/04/2022" => { start: "Hit_the_Road_Jack", end: "International_Commerce_Center" },
+        "05/04/2022" => { start: "Yelyzaveta_Mereshko", end: "Trieste_(bathyscaphe)" },
+        "06/04/2022" => { start: "Wikipédia", end: "Six_Degrees_of_Kevin_Bacon" },
+        "07/04/2022" => { start: "Paléolithique", end: "Colonisation_de_Mars" },
+        "08/04/2022" => { start: "Eunectes_murinus", end: "Cryovolcan" },
+        "09/04/2022" => { start: "Ultra-Trail_du_Mont-Blanc", end: "Vol_en_wingsuit" },
+        "10/04/2022" => { start: "Ouserkaf", end: "Cycle_sexagésimal_chinois" },
     ];
 
 }
