1f567add22127919e8598a958e573a2bb3991b11
Sacramentix <benji.u1225u@gmail.com>
2025-08-28 18:23:19 +0200

    register pwa

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// Register service worker (vite-plugin-pwa)
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 createApp(App).mount('#app')
