631c58dd3367c1dc725b935a629d3a581bfd1873
Sacramentix <benji.u1225u@gmail.com>
2025-08-29 20:23:01 +0200

    signaling server now return timestamp

--- signaling-server/src/index.ts
+++ signaling-server/src/index.ts
@@ -143,11 +143,10 @@ export default {
         }
 
         if (request.method == "HEAD" && room_name == "ping") {
-            return new Response('OK', { status: 200, headers: corsHeaders });
+            return new Response(Date.now().toString(), { status: 200, headers: corsHeaders });
         }
 
         const passwordHash = await hashPassword(password, room_name);
-
 
         const id = env.ROOM.idFromName(passwordHash);
         const stub = env.ROOM.get(id);
