6b627fd4dfd05beddb0860d0b5e14804ef1b6516
Sacramentix <benji.u1225u@gmail.com>
2025-08-30 13:00:25 +0200

    disable safe mode by default

--- src/composables/useLocalSetting.ts
+++ src/composables/useLocalSetting.ts
@@ -1,6 +1,6 @@
 import { useLocalStorage } from "@vueuse/core";
 
-export const safemode = useLocalStorage("safemode", true);
+export const safemode = useLocalStorage("safemode", false);
 
 export function useLocalSetting() {
     return {
